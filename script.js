04bfec6d8a246a6a8f6ed435a64bb88fb630b11f
tarik <tarikozgur0607@gmail.com>
2022-08-19 15:37:15 +0300

    add some code

---
+++ script.js
@@ -0,0 +1,27 @@
+/*
+4işlev oluştur ve test et 
+1. toplama
+2. çıkarma
+3. çarpma 
+4. bölme */
+
+//1
+function sum(a,b) {
+    return a+b;
+}
+//2
+function subtract(a,b) {
+    return a-b;
+}
+//3
+function multiply(a,b) {
+    return a*b;
+}
+//4
+function divide(a,b){
+    return a/b;
+}
+//operate
+function operate(num1,num2) {
+    
+}
